efbiz/efbiz

commit a6f58caf43cb5c2b332d03d16e3be914b6e346ed

Added new "name" argument to the "init" method of the Component interface: now the name of the container (set by the xml attribute container.name) is passed to the init method and it is used to retrieve the container's configuration; before of this the container's name was hardcoded preventing the ability to define (and load) two containers (with different settings) of the same type.
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1355652 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/pos/src/org/ofbiz/pos/container/JposDeviceContainer.java
@@ -31,12 +31,14 @@ public class JposDeviceContainer implements Container {
 
     protected String configFile = null;
 
-    public void init(String[] args, String configFile) throws ContainerException {
+    private String name;
+
+    public void init(String[] args, String name, String configFile) throws ContainerException {
         this.configFile = configFile;
     }
 
     public boolean start() throws ContainerException {
-        ContainerConfig.Container cc = ContainerConfig.getContainer("jpos.device-container", configFile);
+        ContainerConfig.Container cc = ContainerConfig.getContainer(name, configFile);
         if (cc == null) {
             throw new ContainerException("No jpos.device-container configuration found in container config!");
         }

framework/appserver/src/org/ofbiz/appservers/GenerateContainer.java
@@ -75,7 +75,7 @@ public class GenerateContainer implements Container {
     /**
      * @see org.ofbiz.base.container.Container#init(java.lang.String[], java.lang.String)
      */
-    public void init(String[] args, String configFile) {
+    public void init(String[] args, String name, String configFile) {
         ofbizHome = System.getProperty("ofbiz.home");
         this.configFile = configFile;
         this.args = args;

framework/base/src/org/ofbiz/base/container/BeanShellContainer.java
@@ -34,13 +34,15 @@ public class BeanShellContainer implements Container {
 
     protected String configFileLocation = null;
     protected Interpreter bsh = null;
+    protected String containerName;
     protected String name;
     protected int port;
 
     /**
      * @see org.ofbiz.base.container.Container#init(java.lang.String[], java.lang.String)
      */
-    public void init(String[] args, String configFile) {
+    public void init(String[] args, String name, String configFile) {
+        this.containerName = name;
         this.configFileLocation = configFile;
     }
 
@@ -49,7 +51,7 @@ public class BeanShellContainer implements Container {
      */
     public boolean start() throws ContainerException {
         // get the container config
-        ContainerConfig.Container cfg = ContainerConfig.getContainer("beanshell-container", configFileLocation);
+        ContainerConfig.Container cfg = ContainerConfig.getContainer(containerName, configFileLocation);
 
         // get the app-name
         ContainerConfig.Container.Property appName = cfg.getProperty("app-name");

framework/base/src/org/ofbiz/base/container/ClassLoaderContainer.java
@@ -36,7 +36,7 @@ public class ClassLoaderContainer implements Container {
     /**
      * @see org.ofbiz.base.container.Container#init(java.lang.String[], java.lang.String)
      */
-    public void init(String[] args, String configFile) throws ContainerException {
+    public void init(String[] args, String name, String configFile) throws ContainerException {
         ClassLoader parent = Thread.currentThread().getContextClassLoader();
         if (parent == null) {
             parent = Classpath.class.getClassLoader();

framework/base/src/org/ofbiz/base/container/ComponentContainer.java
@@ -57,11 +57,11 @@ public class ComponentContainer implements Container {
     /**
      * @see org.ofbiz.base.container.Container#init(java.lang.String[], java.lang.String)
      */
-    public void init(String[] args, String configFile) throws ContainerException {
+    public void init(String[] args, String name, String configFile) throws ContainerException {
         this.configFileLocation = configFile;
 
         // get the config for this container
-        ContainerConfig.Container cc = ContainerConfig.getContainer("component-container", configFileLocation);
+        ContainerConfig.Container cc = ContainerConfig.getContainer(name, configFileLocation);
 
         // check for an override loader config
         String loaderConfig = null;

framework/base/src/org/ofbiz/base/container/Container.java
@@ -40,12 +40,13 @@ public interface Container {
      * should initialize internal structures and then return.
      *
      * @param args Command-line arguments.
+     * @param name Unique name of the container's instance.
      * @param configFile Location of the configuration file used to load this container.
      * @throws ContainerException If an error was encountered. Throwing this exception
      * will halt container loading, so it should be thrown only when other containers
      * might depend on this one.
      */
-    public void init(String[] args, String configFile) throws ContainerException;
+    public void init(String[] args, String name, String configFile) throws ContainerException;
 
     /**
      * Start the container process. This method must not block - implementations

framework/base/src/org/ofbiz/base/container/ContainerLoader.java
@@ -208,7 +208,7 @@ public class ContainerLoader implements StartupLoader {
 
         // initialize the container object
         try {
-            containerObj.init(args, configFile);
+            containerObj.init(args, containerCfg.name, configFile);
         } catch (ContainerException e) {
             throw new StartupException("Cannot init() " + containerCfg.name, e);
         } catch (java.lang.AbstractMethodError e) {

framework/base/src/org/ofbiz/base/container/GroovyShellContainer.java
@@ -28,6 +28,7 @@ public class GroovyShellContainer implements Container {
 
     private static final Logger log = Logger.getLogger(GroovyShellContainer.class);
 
+    private String name;
     private String configFileLocation = null;
     private GroovyService gsh = null;
 
@@ -35,7 +36,8 @@ public class GroovyShellContainer implements Container {
      * @see org.ofbiz.base.container.Container#init(java.lang.String[], java.lang.String)
      */
     @Override
-    public void init(String[] args, String configFile) {
+    public void init(String[] args, String name, String configFile) {
+        this.name = name;
         configFileLocation = configFile;
     }
 
@@ -63,7 +65,7 @@ public class GroovyShellContainer implements Container {
     }
 
     private int getTelnetPort() throws ContainerException {
-        ContainerConfig.Container config = ContainerConfig.getContainer("groovyshell-container", configFileLocation);
+        ContainerConfig.Container config = ContainerConfig.getContainer(name, configFileLocation);
         Property telnetPort = config.getProperty("telnet-port");
         try {
             return Integer.parseInt(telnetPort.value);

commit 1dc0439c13d2416533aca73dce6c9c033470aca9

Fixes a bug introduced in r1355652
git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1355682 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/pos/src/org/ofbiz/pos/container/JposDeviceContainer.java
@@ -35,7 +35,9 @@ public class JposDeviceContainer implements Container {
 
     public void init(String[] args, String name, String configFile) throws ContainerException {
         this.configFile = configFile;
+        this.name = name;
     }
+
 
     public boolean start() throws ContainerException {
         ContainerConfig.Container cc = ContainerConfig.getContainer(name, configFile);