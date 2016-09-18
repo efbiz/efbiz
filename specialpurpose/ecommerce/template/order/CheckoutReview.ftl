efbiz/efbiz

commit d1299a7ec9b83953961a867d5ee3355dd0d55d66

Improved: E-commerce : FTL formatting. (OFBIZ-7949)
For improved FTL code readability. Here is the first patch for the effort. There are still files which are required to be formatted. 

Thanks: Vaibhav Jain for the contribution.

git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1761135 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/ecommerce/template/order/CheckoutReview.ftl
@@ -18,45 +18,50 @@ under the License.
 -->
 
 <script language="JavaScript" type="text/javascript">
-<!--
-    var clicked = 0;
-    function processOrder() {
-        if (clicked == 0) {
-            clicked++;
-            //window.location.replace("<@ofbizUrl>processorder</@ofbizUrl>");
-            document.${parameters.formNameValue}.processButton.value="${uiLabelMap.OrderSubmittingOrder}";
-            document.${parameters.formNameValue}.processButton.disabled=true;
-            document.${parameters.formNameValue}.submit();
-        } else {
-            showErrorAlert("${uiLabelMap.CommonErrorMessage2}","${uiLabelMap.YoureOrderIsBeingProcessed}");
-        }
+  <!--
+  var clicked = 0;
+  function processOrder() {
+    if (clicked == 0) {
+      clicked++;
+      //window.location.replace("<@ofbizUrl>processorder</@ofbizUrl>
+      ");
+      document.${parameters.formNameValue}.processButton.value = "${uiLabelMap.OrderSubmittingOrder}";
+      document.${parameters.formNameValue}.processButton.disabled = true;
+      document.${parameters.formNameValue}.submit();
+    } else {
+      showErrorAlert("${uiLabelMap.CommonErrorMessage2}", "${uiLabelMap.YoureOrderIsBeingProcessed}");
     }
-// -->
+  }
+  // -->
 </script>
 
 <h1>${uiLabelMap.OrderFinalCheckoutReview}</h1>
-<#if !isDemoStore?? && isDemoStore><p>${uiLabelMap.OrderDemoFrontNote}.</p></#if>
+<#if !isDemoStore?? && isDemoStore>
+  <p>${uiLabelMap.OrderDemoFrontNote}.</p>
+</#if>
 
 <#if cart?? && 0 < cart.size()>
   ${screens.render("component://ecommerce/widget/OrderScreens.xml#orderheader")}
-  <br />
+  <br/>
   ${screens.render("component://ecommerce/widget/OrderScreens.xml#orderitems")}
   <table border="0" cellpadding="1" width="100%">
-   <tr>
+    <tr>
       <td colspan="4">
         &nbsp;
       </td>
       <td align="right">
         <form type="post" action="<@ofbizUrl>processorder</@ofbizUrl>" name="${parameters.formNameValue}">
           <#if (requestParameters.checkoutpage)?has_content>
-            <input type="hidden" name="checkoutpage" value="${requestParameters.checkoutpage}" />
+            <input type="hidden" name="checkoutpage" value="${requestParameters.checkoutpage}"/>
           </#if>
           <#if (requestAttributes.issuerId)?has_content>
-            <input type="hidden" name="issuerId" value="${requestAttributes.issuerId}" />
+            <input type="hidden" name="issuerId" value="${requestAttributes.issuerId}"/>
           </#if>
-          <input type="button" name="processButton" value="${uiLabelMap.OrderSubmitOrder}" onclick="processOrder();" class="mediumSubmit" />
+          <input type="button" name="processButton" value="${uiLabelMap.OrderSubmitOrder}" onclick="processOrder();"
+                 class="mediumSubmit"/>
         </form>
-        <#-- doesn't work with Safari, seems to work with IE, Mozilla <a href="#" onclick="processOrder();" class="buttontextbig">[${uiLabelMap.OrderSubmitOrder}]&nbsp;</a> -->
+      <#-- doesn't work with Safari, seems to work with IE, Mozilla
+           <a href="#" onclick="processOrder();" class="buttontextbig">[${uiLabelMap.OrderSubmitOrder}]&nbsp;</a> -->
       </td>
     </tr>
   </table>

commit 012e192ac64c591c677e7b2bd31ce972f0fd65c5

Improves:E-commerce : FTL formatting (OFBIZ-7949)
Fixes a typo introduced by r1761135 



git-svn-id: https://svn.apache.org/repos/asf/ofbiz/trunk@1761316 13f79535-47bb-0310-9956-ffa450edef68

specialpurpose/ecommerce/template/order/CheckoutReview.ftl
@@ -24,7 +24,6 @@ under the License.
     if (clicked == 0) {
       clicked++;
       //window.location.replace("<@ofbizUrl>processorder</@ofbizUrl>
-      ");
       document.${parameters.formNameValue}.processButton.value = "${uiLabelMap.OrderSubmittingOrder}";
       document.${parameters.formNameValue}.processButton.disabled = true;
       document.${parameters.formNameValue}.submit();